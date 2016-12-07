d0b268b8ead8c79e8a30d4c1fa769a1717f626d9
amogha bhardwaj <amogha bhardwaj>
2016-12-07 13:08:45 +0530

    inital commit

---
+++ views/about.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title> about page</title>
+</head>
+<body>
+<h1>{{pageTitle}}</h1>
+<p> some text</p>
+
+<footer>
+<p> copyright {{currentYear}}</p>
+</footer>
+</body>
+</html>
+
